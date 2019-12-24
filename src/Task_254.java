d505dbdf705a84d5ad3b1c69a4ebadf451269941
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-12-24 10:11:21 +0300

    Edit Task_254, but this format can't go into in ACMP.

--- src/Task_254.java
+++ src/Task_254.java
@@ -1,42 +1,35 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Task_254 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("INPUT.txt"));
         int countGraf = Integer.parseInt(reader.readLine());
         int[] graf = new int[countGraf];
-
         String[] s = reader.readLine().split("\\s");
-
         for (int i = 0; i < s.length; i++) {
             graf[i] = Integer.parseInt(s[i]);
         }
-
         int countStat = Integer.parseInt(reader.readLine());
-
         int[][] arr = new int[countStat][countStat];
-
-
         for (int i = 0; i < countStat; i++) {
             String[] replace = reader.readLine().split("\\s");
             for (int j = 0; j < countStat; j++) {
                 arr[i][j] = Integer.parseInt(replace[j]);
             }
         }
-
-        int[] rez = new int[countGraf];
-        for (int i = 0; i < graf.length-1; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
-                if (graf[i] == arr[i][j]) {
-                    rez[i] = arr[i][j + 1];
-                } else if (graf[i] == arr[i + 1][j]) {
-                    rez[i] = arr[i][j + 1];
-                } else rez[i] = graf[i];
+        PrintWriter out = new PrintWriter("OUTPUT.txt");
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = 0; i < graf.length; i++) {
+                if (graf[i] == arr[j][0]) {
+                    out.print(arr[j][1]);
+                } else if (graf[i] == arr[j + 1][0]) {
+                    out.print(arr[j + 1][1]);
+                } else {
+                    out.print(graf[i]);
+                }
+                if (i != graf.length - 1) out.print(" ");
             }
         }
-
-        int a = 0;
+        out.close();
     }
 }
