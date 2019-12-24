6702cf1d49267ba735c885faa5a6b267de53827f
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-12-24 11:33:54 +0300

    Done Task_254

--- src/Task_254.java
+++ src/Task_254.java
@@ -3,32 +3,33 @@ import java.io.*;
 public class Task_254 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("INPUT.txt"));
-        int countGraf = Integer.parseInt(reader.readLine());
-        int[] graf = new int[countGraf];
+        int n = Integer.parseInt(reader.readLine());
+        int[] before = new int[n];
+        int[] after = new int[n];
+        int last;
+        int next;
         String[] s = reader.readLine().split("\\s");
         for (int i = 0; i < s.length; i++) {
-            graf[i] = Integer.parseInt(s[i]);
+            before[i] = Integer.parseInt(s[i]);
+            after[i] = before[i];
         }
-        int countStat = Integer.parseInt(reader.readLine());
-        int[][] arr = new int[countStat][countStat];
-        for (int i = 0; i < countStat; i++) {
-            String[] replace = reader.readLine().split("\\s");
-            for (int j = 0; j < countStat; j++) {
-                arr[i][j] = Integer.parseInt(replace[j]);
+        int m = Integer.parseInt(reader.readLine());
+        for (int j = 0; j < m; j++) {
+            String[] str = reader.readLine().split("\\s");
+            last = Integer.parseInt(str[0]);
+            next = Integer.parseInt(str[1]);
+            for (int i = 0; i < before.length; i++) {
+                if (before[i] == last) {
+                    after[i] = next;
+                }
             }
         }
         PrintWriter out = new PrintWriter("OUTPUT.txt");
-        for (int j = 0; j < arr.length - 1; j++) {
-            for (int i = 0; i < graf.length; i++) {
-                if (graf[i] == arr[j][0]) {
-                    out.print(arr[j][1]);
-                } else if (graf[i] == arr[j + 1][0]) {
-                    out.print(arr[j + 1][1]);
-                } else {
-                    out.print(graf[i]);
-                }
-                if (i != graf.length - 1) out.print(" ");
-            }
+        for (int i = 0; i < after.length; i++) {
+            if (i != after.length - 1) {
+                out.print(after[i] + " ");
+            } else
+                out.print(after[i]);
         }
         out.close();
     }
