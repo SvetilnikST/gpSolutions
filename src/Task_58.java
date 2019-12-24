e97decbf4e3d6037744ada26e7b180cfcf042ed0
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-12-24 12:28:36 +0300

    Start Task_58

--- src/Task_58.java
+++ src/Task_58.java
@@ -2,45 +2,49 @@ import java.io.*;
 
 public class Task_58 {
     public static void main(String[] args) throws IOException {
-        boolean x = true;
-        String text;
-
         BufferedReader reader = new BufferedReader(new FileReader("INPUT.txt"));
-
         int t = Integer.parseInt(reader.readLine());
-
+        int n;
+        int m;
+        boolean x = true;
+        PrintWriter out = new PrintWriter("OUTPUT.txt");
         for (int i = 0; i < t; i++) {
-            String[] s = reader.readLine().split("\\s");
-            int n = Integer.parseInt(s[0]);
-            int m = Integer.parseInt(s[1]);
+            String[] str = reader.readLine().split("\\s");
+            n = Integer.parseInt(str[0]);
+            m = Integer.parseInt(str[1]);
+            if (n > 2) {
+                //идем по строкам
+                for (int j = 0; j < n - 1; j++) {
+                    //сравниваем значения в текущей и следующей строке. обратить внимание чтобы не вылетить за пределы
+                    String[] current = new String[0];
 
-            if (n > 1 && m > 1) {
-                int[][] array = new int[n][m];
+                    //если значение текущей переменной пустое, то начинаем считывать значение из файла, иначе присваиваем значение предыдушего
+                    if (current!= null) {
+                        current = reader.readLine().split("\\s");
+                        String[] next = reader.readLine().split("\\s");
+                        //проходимся по столбцам и сравнимаем значения в текущем и следующем столбце
+                        for (int k = 0; k < m - 1; k++) {
+                            if (Integer.parseInt(current[k] + current[k + 1] + next[k] + next[k + 1]) % 4 == 0) {
+                                x = false;
+                            }
+                            //текущей строке присвоить значение следующей
+                            current = next;
+                        }
+                    }else {
+                        //текущая переменная пустая и ее нужно запонить
 
-                for (int j = 0; j < n; j++) {
-                    for (int k = 0; k < m; k++) {
-                        array[i][k] = Integer.parseInt(reader.readLine());
-                    }
-                }
-
-                for (int c = 0; c < array.length - 1; c++) {
-                    for (int j = 0; j < array[0].length - 1; j++) {
-                        if ((array[c][j] + array[c][j + 1] + array[c + 1][j] + array[c + 1][j + 1]) % 4 == 0)
-                            x = false;
                     }
                 }
             } else {
+                //пишем в файл ДА
+//                out.print("YES");
                 x = true;
+                reader.readLine();
             }
-
             if (x) {
-                text = "YES";
-            } else {
-                text = "NO";
-            }
-            PrintWriter out = new PrintWriter("OUTPUT.txt");
-            out.print(text);
-            out.close();
+                out.print("YES");
+            } else out.print("NO");
         }
+        out.close();
     }
 }
