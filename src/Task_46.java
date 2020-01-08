d215fd727dc05e1aa6ff301754720d29e95cc01b
SvetilnikST <svetilnik.stezhkina@gmail.com>
2020-01-08 23:52:21 +0300

    Task 46- in process

--- src/Task_46.java
+++ src/Task_46.java
@@ -6,7 +6,6 @@ public class Task_46 {
         int n = Integer.parseInt(bufferedReader.readLine());
         int[] k = new int[]{2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 5, 9, 0, 4, 5, 2, 3, 5, 3, 6, 0, 2, 8, 7, 5};
         String text = "";
-
         if (n == 0) {
             text = "3";
         } else if (n > 0 && n < k.length) {
@@ -20,12 +19,7 @@ public class Task_46 {
                 } else {
                     if (k[i] >= 5) {
                         if (k[i + 1] >= 5) {
-
-                            if (k[i + 1] == 9) {
-
-                                //вопрос конечно что здесь нужно делать
-
-                            } else text += Integer.toString(k[i] + 1);
+                            text += Integer.toString(k[i] + 1);
                         } else text += Integer.toString(k[i]);
                     } else text += Integer.toString(k[i]);
                 }
