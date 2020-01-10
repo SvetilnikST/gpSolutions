2e8757e8bb5823dbf3deff55fa8a4cd19a51fe6a
SvetilnikST <svetilnik.stezhkina@gmail.com>
2020-01-10 18:41:22 +0300

    Task_654 in process

--- src/Task_654.java
+++ src/Task_654.java
@@ -2,35 +2,41 @@ import java.io.*;
 
 public class Task_654 {
     public static void main(String[] args) throws IOException {
+        long n, last, sum, max;
+        String[] s;
+        long[] arr;
         BufferedReader reader = new BufferedReader(new FileReader("INPUT.txt"));
-        int n = Integer.parseInt(reader.readLine());
-        String[] s = reader.readLine().split("\\s");
-        int[] arr = new int[s.length];
-        int last, sum = 0, max = arr[0];
-        int len = s.length - 1;
-        if (n >=1 && n <= 200000 && len >= 1 && len < n) {
-            for (int i = 0; i < s.length; i++) {
-                int k = Integer.parseInt(s[i]);
-                if (k >= 1 && k <= Math.pow(10, 9)) {
-                    arr[i] = k;
+        n = Integer.parseInt(reader.readLine());
+        if (n > 0 && n <= 200000) {
+            s = reader.readLine().split("\\s");
+            if (s.length > 0 && s.length <= n) {
+                arr = new long[s.length];
+                sum = 0;
+                max = arr[0];
+                for (int i = 0; i < s.length; i++) {
+                    long k = Long.parseLong(s[i]);
+                    if (k > 0 && k <= Math.pow(10, 9)) {
+                        arr[i] = k;
+                    }
                 }
+                last = arr[0];
+                for (int i = 1; i < n; i++) {
+                    long cur = arr[i];
+                    if (cur > max) {
+                        max = cur;
+                    }
+                    if (cur > last) {
+                        sum += cur - last;
+                    }
+                    last = cur;
+                }
+                sum += max - last;
+
+                String string = Long.toString(sum);
+                PrintWriter out = new PrintWriter("OUTPUT.txt");
+                out.print(string);
+                out.close();
             }
-            last = arr[0];
-            for (int i = 1; i < n; i++) {
-                int cur = arr[i];
-                if (cur > max) {
-                    max = cur;
-                }
-                if (cur > last) {
-                    sum += cur - last;
-                }
-                last = cur;
-            }
-            sum += max - last;
-            String string = Integer.toString(sum);
-            PrintWriter out = new PrintWriter("OUTPUT.txt");
-            out.print(string);
-            out.close();
         }
     }
 }
