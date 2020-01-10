03c678da75ba2f51555530f5fe8ab69e77ac7f7d
SvetilnikST <svetilnik.stezhkina@gmail.com>
2020-01-10 18:53:45 +0300

    Task_654 in process

--- src/Task_654.java
+++ src/Task_654.java
@@ -2,26 +2,24 @@ import java.io.*;
 
 public class Task_654 {
     public static void main(String[] args) throws IOException {
-        long n, last, sum, max;
-        String[] s;
-        long[] arr;
+        int n;
+        long sum = 0;
         BufferedReader reader = new BufferedReader(new FileReader("INPUT.txt"));
         n = Integer.parseInt(reader.readLine());
         if (n > 0 && n <= 200000) {
-            s = reader.readLine().split("\\s");
+            String[] s = reader.readLine().split("\\s");
             if (s.length > 0 && s.length <= n) {
-                arr = new long[s.length];
-                sum = 0;
-                max = arr[0];
+                int[] arr = new int[s.length];
+                int max = arr[0];
                 for (int i = 0; i < s.length; i++) {
-                    long k = Long.parseLong(s[i]);
+                    int k = Integer.parseInt(s[i]);
                     if (k > 0 && k <= Math.pow(10, 9)) {
                         arr[i] = k;
                     }
                 }
-                last = arr[0];
+                int last = arr[0];
                 for (int i = 1; i < n; i++) {
-                    long cur = arr[i];
+                    int cur = arr[i];
                     if (cur > max) {
                         max = cur;
                     }
