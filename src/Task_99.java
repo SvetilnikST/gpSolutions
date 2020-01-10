a1fa49ff632476dde3ea03799c8d2641bf95a7c5
SvetilnikST <svetilnik.stezhkina@gmail.com>
2020-01-10 12:29:41 +0300

    Done Task_99 start

--- src/Task_99.java
+++ src/Task_99.java
@@ -10,6 +10,10 @@ public class Task_99 {
         int n = Integer.parseInt(s[2]);
 
         int arr[][][] = new int[h][m][n];
+        int mark[][][] = new int[h][m][n];
+
+        int iSt = 0, jSt = 0, kSt = 0;
+        int iEnd = 0, jEnd = 0, kEnd = 0;
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < m; j++) {
@@ -24,14 +28,30 @@ public class Task_99 {
                     }
                     if (str.charAt(k) == 49) {
                         arr[i][j][k] = 1;
+                        iSt = i;
+                        jSt = j;
+                        kSt = k;
+
                     }
                     if (str.charAt(k) == 50) {
                         arr[i][j][k] = 2;
+                        iEnd = i;
+                        jEnd = j;
+                        kEnd = k;
                     }
                 }
             }
             reader.readLine();
         }
+
+        int i = iSt;
+        int j = jSt;
+        int k = kSt;
+
+        int ie = iSt;
+        int je = jSt;
+        int ke = kSt;
+
 
         int a = 0;
     }
