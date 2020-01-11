aa71e268ad303be9f31aaf999594b03d08925d27
SvetilnikST <svetilnik.stezhkina@gmail.com>
2020-01-11 17:49:49 +0300

    Task_99 intresting

--- src/Task_99_newTest.java
+++ src/Task_99_newTest.java
@@ -41,7 +41,6 @@ public class Task_99_newTest {
     int h, m, n, count = 0;
     int arr[][][];
     ArrayList<Coord> arrayList;
-    int rez;
 
     public static void main(String[] args) throws IOException {
         Task_99_newTest app = new Task_99_newTest();
@@ -49,50 +48,57 @@ public class Task_99_newTest {
     }
 
     private void run() throws IOException {
+        long startTime = System.currentTimeMillis();
         BufferedReader reader = new BufferedReader(new FileReader("INPUT.txt"));
         String[] s = reader.readLine().split("\\s");
-        h = Integer.parseInt(s[0]);
-        m = Integer.parseInt(s[1]);
-        n = Integer.parseInt(s[2]);
-        if (h >= 2 && m >= 2 && n >= 2 && h <= 50 && m <= 50 && n <= 50) {
+        Coord start = new Coord();
+        Coord finish = new Coord();
+        int[] dh = {0, 0, 0, 0, 1};
+        int[] dm = {0, 1, -1, 0, 0};
+        int[] dn = {1, 0, 0, -1, 0};
+        h = Integer.parseInt(s[0]) + 2;
+        m = Integer.parseInt(s[1]) + 2;
+        n = Integer.parseInt(s[2]) + 2;
+        if (h - 2 >= 2 && m - 2 >= 2 && n - 2 >= 2 && h - 2 <= 50 && m - 2 <= 50 && n - 2 <= 50) {
             arr = new int[h][m][n];
-            Coord start = new Coord();
-            Coord finish = new Coord();
-            int[] dh = {0, 0, 0, 0, 1};
-            int[] dm = {0, 1, -1, 0, 0};
-            int[] dn = {1, 0, 0, -1, 0};
             String str = "";
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < m; j++) {
-                    str = reader.readLine();
+                    if (j != 0 && j != m - 1 && i != 0 && i != h - 1) {
+                        str = reader.readLine();
+                    }
                     for (int k = 0; k < n; k++) {
-                        if (str.charAt(k) == 46) {
-                            arr[i][j][k] = 0;
-                        }
-                        if (str.charAt(k) == 111) {
+                        if (j == 0 || j == m - 1 || k == 0 || k == n - 1 || i == 0 || i == h - 1) {
                             arr[i][j][k] = -1;
-                        }
-                        if (str.charAt(k) == 49) {
-                            arr[i][j][k] = 0;
-                            start.setH(i);
-                            start.setM(j);
-                            start.setN(k);
-
-                        }
-                        if (str.charAt(k) == 50) {
-                            arr[i][j][k] = 0;
-                            finish.setH(i);
-                            finish.setM(j);
-                            finish.setN(k);
+                        } else {
+                            if (str.charAt(k - 1) == 46) {
+                                arr[i][j][k] = 0;
+                            }
+                            if (str.charAt(k - 1) == 111) {
+                                arr[i][j][k] = -1;
+                            }
+                            if (str.charAt(k - 1) == 49 && i == 1) {
+                                arr[i][j][k] = 0;
+                                start.setH(i);
+                                start.setM(j);
+                                start.setN(k);
+                            }
+                            if (str.charAt(k - 1) == 50 && i == h - 2) {
+                                arr[i][j][k] = 0;
+                                finish.setH(i);
+                                finish.setM(j);
+                                finish.setN(k);
+                            }
                         }
                     }
                 }
-                reader.readLine();
+                if (i != 0 && i != h - 1) {
+                    reader.readLine();
+                }
             }
             arrayList = new ArrayList<>();
             arrayList.add(start);
 
-            rez = 0;
             while (!arrayList.isEmpty()) {
                 Coord cur = new Coord();
                 cur.setH(arrayList.get(arrayList.size() - 1).getH());
@@ -105,16 +111,15 @@ public class Task_99_newTest {
                     tmp.setH(cur.h + dh[i]);
                     tmp.setM(cur.m + dm[i]);
                     tmp.setN(cur.n + dn[i]);
-                    if (ok(tmp)) {
-                        if (arr[tmp.getH()][tmp.getM()][tmp.getN()] <= arr[cur.getH()][cur.getM()][cur.getN()] + 1) {
+                    if (tmp != start) {
+                        if (arr[tmp.getH()][tmp.getM()][tmp.getN()] == 0 || arr[tmp.getH()][tmp.getM()][tmp.getN()] > arr[cur.getH()][cur.getM()][cur.getN()] + 1) {
                             arrayList.add(tmp);
                             arr[tmp.getH()][tmp.getM()][tmp.getN()] = arr[cur.getH()][cur.getM()][cur.getN()] + 1;
                         }
                     }
                 }
             }
-            count = arr[finish.getH()][finish.getM()][finish.getH()] * 5;
-
+            count = arr[finish.getH()][finish.getM()][finish.getN()] * 5;
             if (count != 0) {
                 String string = Integer.toString(count);
                 PrintWriter out = new PrintWriter("OUTPUT.txt");
@@ -122,14 +127,6 @@ public class Task_99_newTest {
                 out.close();
             }
         }
-    }
-
-    boolean ok(Coord c) {
-        if (c.getH() >= 0 && c.getH() < h)
-            if (c.getM() >= 0 && c.getM() < m)
-                if (c.getN() >= 0 && c.getN() < n)
-                    if (arr[c.getH()][c.getM()][c.getN()] == 0)
-                        return true;
-        return false;
+        System.out.println(System.currentTimeMillis() - startTime);
     }
 }
